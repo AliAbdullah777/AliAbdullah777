b28ab22bee98b24a07be188ac7e28cf96d1e294b
AliAbullah <alyabdullahpro@gmail.com>
2022-08-22 11:15:07 +0500

    ch 3 string data type

---
+++ 2str.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    string st1(5, 'A');
+    string st2(5, 'l');
+    string st3(5, 'i');
+    cout << st1 << st2 << st3;
+    return 0;
+
+}
---
+++ str.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main ()
+{
+    string str = "Hello";
+    string phrase = "Hello World";
+    string slang = "Ali Abdullah";
+   
+    cout << str << phrase <<  slang << endl;
+    return 0;
+}
